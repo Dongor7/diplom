0b3d7818e4b87de1ce339df61154b122d3ab69ac
Denis Sidorenko <dongor7@gmail.com>
2017-05-12 00:31:23 +0300

    qwe

--- src/main/java/com/itsupportme/gis/controller/calendar/CalendarController.java
+++ src/main/java/com/itsupportme/gis/controller/calendar/CalendarController.java
@@ -16,31 +16,32 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-@RequestMapping(value = "/calendar")
+//@RequestMapping(value = "/calendar")
 public class CalendarController {
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(value = "/calendar", method = RequestMethod.GET)
     public String main() {
 
         return "global/calendar";
     }
 
-    @RequestMapping(value="/CalendarJsonEvent",  method= RequestMethod.GET)
+    @RequestMapping(value="/calendar/CalendarJsonEvent",  method= RequestMethod.GET)
     public  @ResponseBody void showCalendarDetails(HttpServletResponse response) throws IOException {
 
+        System.out.println("ENTER CALENDAR !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 
         List l = new ArrayList();
 
         Calendar c = new Calendar();
-        c.setId(1);
+        c.setId(0);
         c.setStart("2017-04-23");
         c.setEnd("2017-04-27");
         c.setTitle("Task in Progress");
 
         Calendar d = new Calendar();
-        c.setId(2);
+        c.setId(1);
         c.setStart("2017-04-15");
-        c.setEnd("2017-04-16");
+        c.setEnd("2017-04-17");
         c.setTitle("Task in Progress");
 
         l.add(c);
@@ -50,28 +51,5 @@ public class CalendarController {
         response.setCharacterEncoding("UTF-8");
         response.getWriter().write(new Gson().toJson(l));
 
-
-/*
-
-        Map<String, Object> map = new HashMap<String, Object>();
-        map.put("id", 111);
-        map.put("title", "event1");
-        map.put("start", "2011-07-28");
-        map.put("url", "http://yahoo.com/");
-
-        // Convert to JSON string.
-        String json = new Gson().toJson(map);
-        logger.info(json);
-
-        // Write JSON string.
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
-        try {
-            response.getWriter().write(json);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-*/
     }
 }
